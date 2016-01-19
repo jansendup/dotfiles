976fbb53a9ac255a97d53b9f3adf75ea68e0fc07
Jansen du Plessis <jansendup@gmail.com>
2016-01-19 19:41:43 +0200

    Add Oh-My-Zsh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-DOTFILES=$HOME/.dotfiles
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DOTFILES="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 echo "creating symlinks"
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
 for file in $linkables ; do
-   target="$HOME/.$( basename $file ".symlink" )"
-   echo "creating symlink for $file"
-   ln -s $file $target
+  target="$HOME/.$( basename $file ".symlink" )"
+  [ -e $target ] && cp -r $target "$target.bak" && rm -rf $target
+  echo "creating symlink for $file"
+  ln -s $file $target
 done
